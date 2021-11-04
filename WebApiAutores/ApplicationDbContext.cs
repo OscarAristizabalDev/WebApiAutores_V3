b0e8cfa145ad870cde7ae4499113af392062d69d
OscarAristizabalDev <oaristizabal@dataifx.com>
2021-11-03 20:50:11 -0500

    prueba

--- WebApiAutores/ApplicationDbContext.cs
+++ WebApiAutores/ApplicationDbContext.cs
@@ -12,6 +12,7 @@ public class ApplicationDbContext: IdentityDbContext
 
     }
 
+
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
         base.OnModelCreating(modelBuilder);
